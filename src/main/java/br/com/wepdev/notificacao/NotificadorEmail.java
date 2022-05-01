f7e5d12262480343c00df50ef7244826d476d082
wep1980 <wepbike@gmail.com>
2022-05-01 09:00:52 -0300

    Conhecendo pontos de injeção com @Autowired

--- src/main/java/br/com/wepdev/notificacao/NotificadorEmail.java
+++ src/main/java/br/com/wepdev/notificacao/NotificadorEmail.java
@@ -1,38 +1,21 @@
 package br.com.wepdev.notificacao;
 
+import org.springframework.stereotype.Component;
+
 import br.com.wepdev.modelo.Cliente;
 
-//@Component ao retirar o @Component e necessario dizer para o spring a nova forma de instanciar essa classe
+@Component
 public class NotificadorEmail implements Notificador {
 	
-	private boolean caixaAlta;// por padrão aqui ja é false
-	private String hostServidorSmtp; // servidor de email
 
-	/*
-	 * Com esse construtor se testa o bean, o resultado aparece no console
-	 * receber o servidor smtp como parametro
-	 */
-	public NotificadorEmail(String hostServidorSmtp) {
-		this.hostServidorSmtp = hostServidorSmtp;
-		System.out.println("NotificadorEmail");
-	}
-	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		
-		if(this.caixaAlta) { // se caixa alta for true
-			mensagem = mensagem.toUpperCase(); // transforma toda mensagem em letra maiuscula.
-		}
-		System.out.printf("Notificando %s atrav�s do e-mail %s usando SMTP %s: %s\n", 
+	
+		System.out.printf("Notificando %s atrav�s do e-mail %s: %s\n", 
 				cliente.getNome(), 
 				cliente.getEmail(), 
-				this.hostServidorSmtp,
 				mensagem);
 	}
-	
-	
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
-	}
+
 
 }
--- src/main/java/br/com/wepdev/service/AtivacaoClienteService.java
+++ src/main/java/br/com/wepdev/service/AtivacaoClienteService.java
@@ -1,24 +1,40 @@
 package br.com.wepdev.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.wepdev.modelo.Cliente;
 import br.com.wepdev.notificacao.Notificador;
 
-//@Component // Definindo a classe como um Bean spring, o spring gerencia, instancia , configura e injeta objetos dessa classe em outros beans
+@Component // Definindo a classe como um Bean spring, o spring gerencia, instancia , configura e injeta objetos dessa classe em outros beans
 public class AtivacaoClienteService {
 	
-	
+	@Autowired
 	private Notificador notificador;
 	
+	/*
+	 * O Construtor e sempre uma opção de ponto de injeção.
+	 * Em alguns lugares essa opção e bastante utilizada, pois facilita bastante nos testes
+	 */
+//	@Autowired
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+//	
+//	/*
+//	 * Ao gerar um novo construtor o spring gera um erro, pois ele não sabe qual construtor utilizar,
+//	 * para resolver o problema foi utilizado a anotação @Autowired no construtor acima
+//	 */
+//	public AtivacaoClienteService(String qualquerCoisa) {
+//		
+//	}
+
+//	@Autowired
+//	public void setNotificador(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+
 	
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService" + notificador);
-	}
-
-
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		
