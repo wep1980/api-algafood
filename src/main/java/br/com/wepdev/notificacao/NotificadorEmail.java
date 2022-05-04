4306d8445e11e76e10b94f852ba0691534a27833
wep1980 <wepbike@gmail.com>
2022-05-04 10:54:59 -0300

    Acessando propriedades criando uma classe e utilizando @ConfigurationProperties

--- src/main/java/br/com/wepdev/notificacao/NotificadorEmail.java
+++ src/main/java/br/com/wepdev/notificacao/NotificadorEmail.java
@@ -1,6 +1,6 @@
 package br.com.wepdev.notificacao;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.wepdev.modelo.Cliente;
@@ -9,18 +9,15 @@ import br.com.wepdev.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	@Value("${notificador.email.host-servidor}") // pegando os valores que foram definidos no application.properties
-	private String host;
-	
-	@Value("${notificador.email.porta-servidor}") // pegando os valores que foram definidos no application.properties
-	private Integer porta;
 
+	@Autowired
+	private NotificadorProperties properties;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		
-		System.out.println("Host: " + host);
-		System.out.println("Porta: " + porta);
+		System.out.println("Host: " + properties.getHostServidor());
+		System.out.println("Porta: " + properties.getPortaServidor());
 		
 		System.out.printf("Notificando %s atrav�s do e-mail %s: %s\n", 
 				cliente.getNome(), 
